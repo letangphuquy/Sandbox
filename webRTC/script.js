056e7c4c9f9d56ed8b017517dd4a6cd2896a7f1b
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-11-15 19:14:07 +0700

    clone official samples & some testing
    it turned out that my code is not blatantly wrong, but something wrong when run code locally (Brave issue?)

--- webRTC/script.js
+++ webRTC/script.js
@@ -1,29 +1,37 @@
+`use strict`
 const video = document.querySelector("video")
-let htmlList = document.querySelector("section#info ul");
+let listElement = document.querySelector("section#info ul");
+console.log(listElement)
 
 const openMediaDevice = async (constraints) => {
     return navigator.mediaDevices.getUserMedia(constraints)
 }
-
+// HTMLMediaElement.prototype
 const listDevices = async() => {
     const devices = await navigator.mediaDevices.enumerateDevices()
-    console.log('got ' + devices)
-    for (const device of devices) {
-        let li = document.createElement("li");
-        // console.log(device)
-        const text = `${device.kind} device: ${device.label}, id = ${device.deviceId}`;
-        console.log(text)
-        li.textContent = text;
-        htmlList.appendChild(li);
-    }
+    console.log('query devices ' + devices)
+    // navigator.mediaDevices.enumerateDevices().then(
+    //     (devices) => {
+            for (const device of devices) {
+                console.log('\ta device: ' + device + " " + Object.getOwnPropertyNames(device))
+                let li = document.createElement("li");
+                // console.log(device)
+                const text = `${device.kind} device: ${device.label}, id = ${device.deviceId}`;
+                console.log(text)
+                li.textContent = text;
+                listElement.appendChild(li);
+            }
+    // })
+}
+
+const constraints = {
+    'video': true,
+    'audio': true
 }
 
 const driverFun = async() => {
     try {
-        const stream = await openMediaDevice({
-            'video': true,
-            'audio': true
-        });
+        const stream = await openMediaDevice(constraints);
         window.stream = stream
         video.srcObject = stream
         console.log('got ' + stream) 
@@ -33,17 +41,35 @@ const driverFun = async() => {
             console.log('track ', track)
         }
     } catch (error) {
-        console.error("WA! " + error)
+        console.error("WA! " + error.name + ": " + error.message)
     }
 }
 
 // driverFun()
+
+function gotStream(stream) {
+    return navigator.mediaDevices.enumerateDevices()
+} 
+
+function gotDevices(deviceInfos) {
+    for (const info of deviceInfos) {
+        console.log("\tinfo: " + info.kind + " " + info.deviceId + " " + info.label)
+    }
+}
+function handleError(error) {
+    console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+  }
+
+navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+
 
 navigator.mediaDevices.addEventListener("devicechange", event => {
     console.log("Detected event " + event);
     listDevices()
 })
 //OOP section. Researching OOJS and typescript 
+
+/*
 class Cell {
     static category = "biology"
     name = 'an unnamed cell'
@@ -65,3 +91,4 @@ class Virus extends Cell {
 
 let virus = new Virus()
 virus.display()
+*/
