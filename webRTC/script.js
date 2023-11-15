0c81c46252ac485003362b08852593a0e69da2a6
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-11-15 15:33:43 +0700

    JS OOP
    tryin'

--- webRTC/script.js
+++ webRTC/script.js
@@ -1,15 +1,13 @@
 const video = document.querySelector("video")
+let htmlList = document.querySelector("section#info ul");
 
 const openMediaDevice = async (constraints) => {
     return navigator.mediaDevices.getUserMedia(constraints)
 }
 
-
-let htmlList = document.querySelector("section#info ul");
-
 const listDevices = async() => {
     const devices = await navigator.mediaDevices.enumerateDevices()
-    
+    console.log('got ' + devices)
     for (const device of devices) {
         let li = document.createElement("li");
         // console.log(device)
@@ -22,21 +20,48 @@ const listDevices = async() => {
 
 const driverFun = async() => {
     try {
-        // await listDevices()
         const stream = await openMediaDevice({
             'video': true,
-            'audio': false
+            'audio': true
         });
+        window.stream = stream
         video.srcObject = stream
         console.log('got ' + stream) 
+        listDevices()
+        const tracks = stream.getTracks()
+        for (const track of tracks) {
+            console.log('track ', track)
+        }
     } catch (error) {
         console.error("WA! " + error)
     }
 }
 
-driverFun()
+// driverFun()
 
 navigator.mediaDevices.addEventListener("devicechange", event => {
     console.log("Detected event " + event);
     listDevices()
 })
+//OOP section. Researching OOJS and typescript 
+class Cell {
+    static category = "biology"
+    name = 'an unnamed cell'
+    constructor(name) {
+        if (name)
+        this.name = name
+    }
+    display() {
+        console.log(`Hello, this is ${this.name}. I'm classified as ${Cell.category}`)
+    }
+}
+
+class Virus extends Cell {
+    display() {
+        console.log("Virus greeting. Overriding")
+        super.display()
+    }
+}
+
+let virus = new Virus()
+virus.display()
